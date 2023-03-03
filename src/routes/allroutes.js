4c13018faddb9162fa3245f46078c85e5a44f164
shema jolivet gislain <shemajolivet@gmail.com>
2023-03-03 07:08:56 +0200

    delete un needed file

--- src/routes/allroutes.js
+++ src/routes/allroutes.js
@@ -4,11 +4,13 @@ import userRoute from "./user_route.js"
 import contactRoute from "./querieroute.js";
 import loginRoute from "./loginroute.js";
 import commentRoute from "./comment.route.js";
+// import logoutRoute from "./logout.route.js";
 const router = express.Router();
 
 router.use("/blogs", blogRoute);
 router.use("/users", userRoute);
 router.use("/queries", contactRoute);
 router.use("/login",loginRoute);
+// router.use("/logout", logoutRoute);
 router.use("/", commentRoute);
 export default router;
---
+++ src/routes/logout.route.js
@@ -0,0 +1,6 @@
+// import express from "express";
+// import logout from "../controllers/logout.controllers.js";
+
+// const router = express.Router();
+// router.post("/", logout.logout);
+// export default router;
