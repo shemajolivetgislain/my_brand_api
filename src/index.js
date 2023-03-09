ff0907f6e146c40f65c5285d4c1ac5126eaf9039
shema jolivet gislain <shemajolivet@gmail.com>
2023-03-09 16:21:12 +0200

    repair index.js

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ import mongoose from "mongoose";
 import allRoutes from "./routes/allroutes.js";
 import response from "./utils/response.utils.js";
 import cookieParser from "cookie-parser";
+import swaggerUI from "swagger-ui-express";
+import docs from "./documentations/index.js";
+
+
 mongoose.set("strictQuery", true);
 
 // configuring dotenv
@@ -22,6 +26,8 @@ app.use(bodyParser.json());
 app.get("/", (req, res) =>
   response.success(res, 200, "Welcome To My_brand Backend [API]")
 );
+// api documentation
+app.use("/docs", swaggerUI.serve, swaggerUI.setup(docs));
 app.use(allRoutes);
 const port = process.env.PORT;
 mongoose.set("strictQuery", true);
